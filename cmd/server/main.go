eee32876baf8fb33d1f13b4aaf5b198ac98a2e8b
Nikolai Korolev <korolev.n@hotmail.com>
2025-06-04 11:24:11 +0300

    feat: добавить подключение к БД

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/gin-gonic/gin"
-
+	"github.com/korolev-n/merch-auth/internal/config"
 	"github.com/korolev-n/merch-auth/internal/repository/mocks"
 	"github.com/korolev-n/merch-auth/internal/service"
-
 	transport "github.com/korolev-n/merch-auth/internal/transport/http"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	// db, _ := sql.Open("postgres", "postgres://user:pass@localhost:5432/dbname?sslmode=disable")
-	// defer db.Close()
+
+	cfg := config.NewConfig()
+
+	db, err := openDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	// userRepo := repository.NewUserRepository(db)
 	// walletRepo := repository.NewWalletRepository(db)
@@ -29,4 +36,13 @@ func main() {
 	if err := r.Run(); err != nil {
 		log.Fatalf("could not start server: %v", err)
 	}
+}
+
+func openDB(cfg *config.Config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.DBdsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
---
+++ internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBdsn string
+}
+
+func NewConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
+	dsn := os.Getenv("MERCH_DB_DSN")
+	if dsn == "" {
+		log.Println("environment variable MERCH_DB_DSN is not set") // заменить на Fatal()
+	}
+	return &Config{
+		DBdsn: dsn,
+	}
+}
