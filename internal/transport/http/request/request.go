bc9baebaa0f8d973357e520c99b504b4e15a3d9b
Nikolai Korolev <korolev.n@hotmail.com>
2025-06-10 13:44:35 +0300

    feat: добавить структуру входящего запроса

--- internal/transport/http/request/request.go
+++ internal/transport/http/request/request.go
@@ -4,3 +4,8 @@ type AuthRequest struct {
     Username string `json:"username"`
     Password string `json:"password"`
 }
+
+type SendCoinRequest struct {
+	ToUser string `json:"toUser"`
+	Amount int    `json:"amount"`
+}
