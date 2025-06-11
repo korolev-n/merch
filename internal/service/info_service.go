2fa501b4956e5be0300e6a572a0231fdbc0e2b15
Nikolai Korolev <korolev.n@hotmail.com>
2025-06-11 12:14:23 +0300

    feat: добавить сервис получения отчета

---
+++ internal/service/info_service.go
@@ -0,0 +1,51 @@
+package service
+
+import (
+	"context"
+
+	"github.com/korolev-n/merch-auth/internal/domain"
+	"github.com/korolev-n/merch-auth/internal/repository"
+)
+
+type Info interface {
+	GetUserInfo(ctx context.Context, userID int) (*domain.InfoResponse, error)
+}
+
+type InfoService struct {
+	Repo repository.InfoRepository
+}
+
+func NewInfoService(repo repository.InfoRepository) *InfoService {
+	return &InfoService{Repo: repo}
+}
+
+func (s *InfoService) GetUserInfo(ctx context.Context, userID int) (*domain.InfoResponse, error) {
+	balance, err := s.Repo.GetWalletBalance(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	inventory, err := s.Repo.GetUserInventory(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	received, err := s.Repo.GetReceivedCoins(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	sent, err := s.Repo.GetSentCoins(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &domain.InfoResponse{
+		Coins:     balance,
+		Inventory: inventory,
+		CoinHistory: domain.CoinHistory{
+			Received: received,
+			Sent:     sent,
+		},
+	}, nil
+}
