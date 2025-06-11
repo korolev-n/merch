eb7e57a3c5c22c185effadb3ad1a32fd170a7582
Nikolai Korolev <korolev.n@hotmail.com>
2025-06-11 09:47:08 +0300

    refactor: улучшить процесс миграции

--- migrations/000003_create_coin_transactions_table.up.sql
+++ migrations/000003_create_coin_transactions_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE coin_transactions (
+CREATE TABLE IF NOT EXISTS coin_transactions (
     id SERIAL PRIMARY KEY,
     from_user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE SET NULL,
     to_user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE SET NULL,
