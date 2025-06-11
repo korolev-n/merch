2965bf3c4c0ad8e37fa77c7b17f1ace278301ff9
Nikolai Korolev <korolev.n@hotmail.com>
2025-06-11 12:19:14 +0300

    feat: добавить структуру ответа для отчета

--- internal/transport/http/response/response.go
+++ internal/transport/http/response/response.go
@@ -7,3 +7,29 @@ type ErrorResponse struct {
 type AuthResponse struct {
     Token string `json:"token"`
 }
+
+type InventoryItem struct {
+	Type     string `json:"type"`
+	Quantity int    `json:"quantity"`
+}
+
+type CoinReceived struct {
+	FromUser string `json:"fromUser"`
+	Amount   int    `json:"amount"`
+}
+
+type CoinSent struct {
+	ToUser string `json:"toUser"`
+	Amount int    `json:"amount"`
+}
+
+type CoinHistory struct {
+	Received []CoinReceived `json:"received"`
+	Sent     []CoinSent     `json:"sent"`
+}
+
+type InfoResponse struct {
+	Coins       int            `json:"coins"`
+	Inventory   []InventoryItem `json:"inventory"`
+	CoinHistory CoinHistory    `json:"coinHistory"`
+}
