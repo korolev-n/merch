e9cc8dfac39eda7d1f117a1cd8d15aeedde55adb
Nikolai Korolev <korolev.n@hotmail.com>
2025-06-11 10:51:55 +0300

    feat: добавить ошибки для сервиса покупки

--- internal/service/errors.go
+++ internal/service/errors.go
@@ -3,11 +3,12 @@ package service
 import "errors"
 
 var (
-	ErrIncorrectPassword = errors.New("incorrect password")
-	ErrUserAlreadyExists = errors.New("user already exists")
-	ErrUserNotFound      = errors.New("user not found")
-	ErrTokenGeneration   = errors.New("could not generate token")
-	ErrInvalidToken      = errors.New("invalid JWT token")
+	ErrIncorrectPassword   = errors.New("incorrect password")
+	ErrUserAlreadyExists   = errors.New("user already exists")
+	ErrUserNotFound        = errors.New("user not found")
+	ErrTokenGeneration     = errors.New("could not generate token")
+	ErrInvalidToken        = errors.New("invalid JWT token")
 	ErrInsufficientBalance = errors.New("insufficient balance")
-	ErrNegativeBalance = errors.New("amount must be greater than zero")
+	ErrNegativeBalance     = errors.New("amount must be greater than zero")
+	ErrItemNotFound        = errors.New("item not found")
 )
